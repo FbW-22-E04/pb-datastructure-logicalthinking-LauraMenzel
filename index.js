1c141fbcb6baeb1d9541322f24e4d4e5f5775b81
LauraMenzel <menzel-laura@gmx.de>
2022-08-23 14:49:39 +0200

    adding sort

---
+++ index.js
@@ -0,0 +1,21 @@
+//1
+
+let numArray = [4, 2, 5, 3, 9, 7, 1, 8, 2, 6];
+numArray = numArray.sort(function (a, b) {
+  return a - b;
+});
+console.log(numArray);
+console.log("-----------------------------------------------------------------");
+
+//2
+
+
+let nameArray = ["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
+
+function sortedArray(array){
+
+return array.sort()
+}
+
+
+console.log(sortedArray(nameArray));
